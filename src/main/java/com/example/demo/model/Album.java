57a0f68526bb642a5aafa2989c89b6bca314c713
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-03-12 10:30:00 +0100

    creamos la clase Album para su implementacion

--- src/main/java/com/example/demo/model/Album.java
+++ src/main/java/com/example/demo/model/Album.java
@@ -1,4 +1,60 @@
 package com.example.demo.model;
 
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Document(collection = "album")
 public class Album {
+
+    @Id
+    private int id;
+    private int grupoId;
+    private String titulo;
+    private Date data_lanzamento;
+    private BigDecimal puntuacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGrupoId() {
+        return grupoId;
+    }
+
+    public void setGrupoId(int grupoId) {
+        this.grupoId = grupoId;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getData_lanzamento() {
+        return data_lanzamento;
+    }
+
+    public void setData_lanzamento(Date data_lanzamento) {
+        this.data_lanzamento = data_lanzamento;
+    }
+
+    public BigDecimal getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(BigDecimal puntuacion) {
+        this.puntuacion = puntuacion;
+    }
 }
