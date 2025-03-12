06e353879f155f6fecd97b2c46328b09efb267c4
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-03-12 10:37:31 +0100

    Creamos la clase Grupo con todos sus datos

--- src/main/java/com/example/demo/model/Grupo.java
+++ src/main/java/com/example/demo/model/Grupo.java
@@ -1,4 +1,47 @@
 package com.example.demo.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+@Document(collection = "grupo")
 public class Grupo {
+
+    @Id
+    private int id;
+    private String nome;
+    private String xenero;
+    private Date data_formacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getXenero() {
+        return xenero;
+    }
+
+    public void setXenero(String xenero) {
+        this.xenero = xenero;
+    }
+
+    public Date getData_formacion() {
+        return data_formacion;
+    }
+
+    public void setData_formacion(Date data_formacion) {
+        this.data_formacion = data_formacion;
+    }
 }
