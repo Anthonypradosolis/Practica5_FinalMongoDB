ef96aec79d8d836d0c494485862dafd7a53b0859
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-03-12 09:25:07 +0100

    Subiendo lo primero

---
+++ src/main/java/com/example/demo/Practica5FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica5FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica5FinalApplication.class, args);
+	}
+
+}
