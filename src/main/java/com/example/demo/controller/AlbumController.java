030203dfce13a005912152f6c343f21dc36f4134
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-03-13 14:06:36 +0100

    añadimos un atributo mas un metodo

--- src/main/java/com/example/demo/controller/AlbumController.java
+++ src/main/java/com/example/demo/controller/AlbumController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controller;
 
+
+import com.example.demo.model.Album;
+import com.example.demo.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/album")
 public class AlbumController {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    @PostMapping("/crear")
+    public Album crearAlbum(@RequestBody Album album) {
+        return albumRepository.save(album);
+    }
+
 }
