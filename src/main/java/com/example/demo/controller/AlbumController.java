f4a669d990fd638068fb48f6a06276788cd6fe70
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-03-13 14:19:39 +0100

    añadimos otros metodos, sin acabar

--- src/main/java/com/example/demo/controller/AlbumController.java
+++ src/main/java/com/example/demo/controller/AlbumController.java
@@ -6,6 +6,9 @@ import com.example.demo.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/album")
 public class AlbumController {
@@ -22,5 +25,22 @@ public class AlbumController {
     public void eliminarAlbum(@PathVariable String id) {
         albumRepository.deleteById(id);
     }
+
+    @PutMapping("/actualizar/{id}")
+    public Album updateAlbum(@PathVariable String id, @RequestBody Album album) {
+
+        return albumRepository.save(album);
+    }
+
+    @GetMapping("/listar")
+    public List<Album> listAlbumns(){
+        return albumRepository.findAll();
+    }
+
+    @GetMapping("/listar/{id}")
+    public Optional<Album> listAlbumns(@PathVariable String id){
+        return albumRepository.findById(id);
+    }
+
 
 }
