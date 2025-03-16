b3f601114b123269b1ac1945eb1e8a9bb149943e
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 13:51:50 +0100

    creamos todos los metodos

--- src/main/java/com/example/demo/Psql/service/AlbumServices.java
+++ src/main/java/com/example/demo/Psql/service/AlbumServices.java
@@ -1,8 +1,14 @@
 package com.example.demo.Psql.service;
 
 import com.example.demo.Psql.ComunicacionMongo.ComunicacionMongo;
+import com.example.demo.Psql.model.Album;
+import com.example.demo.Psql.model.AlbumMongo;
 import com.example.demo.Psql.repository.AlbumRepository;
 import com.example.demo.Psql.repository.GrupoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
 
 public class AlbumServices {
 
@@ -16,6 +22,42 @@ public class AlbumServices {
         this.comunicacionMongo = comunicacionMongo;
     }
 
+    public ResponseEntity<Album> addAlbum(Album album){
+        if(grupoRepository.findById(album.getGrupo_id().getId()).isPresent()){
+            albumRepository.save(album);
+            return ResponseEntity.status(HttpStatus.CREATED).body(album);
+        }
+        return null;
+    }
 
+    public ResponseEntity deleteAlbum(Integer id){
+        Optional<Album> albumOptional = albumRepository.findById(id);
+        if(albumOptional.isPresent()){
+            albumRepository.delete(albumOptional.get());
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    public ResponseEntity<Album> crearAlbum(Album album){
+        if(grupoRepository.findById(album.getGrupo_id().getId()).isPresent()){
+            albumRepository.saveAndFlush(album);
+            AlbumMongo albumMongo = new AlbumMongo(album.getId(),album.getGrupo_id().getId(), album.getTitulo(), album.getData_lanzamento(),album.getPuntuacion());
+            comunicacionMongo.crearAlbum(albumMongo);
+            return ResponseEntity.status(HttpStatus.CREATED).body(album);
+        }
+        return null;
+    }
+
+    public ResponseEntity borrarAlbum(Integer id){
+        Optional<Album> albumOptional =albumRepository.findById(id);
+        if(albumOptional.isPresent()){
+            Integer idAlbum = albumOptional.get().getId();
+            albumRepository.delete(albumOptional.get());
+            comunicacionMongo.borrarAlbum(idAlbum);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
 
 }
