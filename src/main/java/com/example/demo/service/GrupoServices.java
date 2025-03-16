43d7ac669fa03a18bc396a70aedba7aa7a187f6b
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 11:47:55 +0100

    creamos el metodo crearGrupo

--- src/main/java/com/example/demo/service/GrupoServices.java
+++ src/main/java/com/example/demo/service/GrupoServices.java
@@ -1,4 +1,22 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Grupo;
+import com.example.demo.repository.GrupoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GrupoServices {
+
+    private final GrupoRepository grupoRepository;
+
+    public GrupoServices(GrupoRepository grupoRepository) {
+        this.grupoRepository = grupoRepository;
+    }
+
+    public ResponseEntity<Grupo> crearGrupo(Grupo grupo){
+        grupoRepository.insert(grupo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(grupo);
+    }
 }
