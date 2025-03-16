d5f33d9465a32556bf3f93cee705edb3b038b3aa
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 11:40:02 +0100

    borramos un import

--- src/main/java/com/example/demo/repository/AlbumRepository.java
+++ src/main/java/com/example/demo/repository/AlbumRepository.java
@@ -2,8 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Album;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AlbumRepository extends MongoRepository<Album,Integer> {
 }
