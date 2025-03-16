e6b37b0c1558a4bbe1264473a7cbf467508cebf7
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 11:05:46 +0100

    Creamos esta clase y empezamos con el metodo crearAlbum

--- src/main/java/com/example/demo/service/AlbumServices.java
+++ src/main/java/com/example/demo/service/AlbumServices.java
@@ -1,4 +1,28 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Album;
+import com.example.demo.repository.AlbumRepository;
+import com.example.demo.repository.GrupoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AlbumServices {
+
+    private final AlbumRepository albumRepository;
+    private final GrupoRepository grupoRepository;
+
+    public AlbumServices(AlbumRepository albumRepository, GrupoRepository grupoRepository) {
+        this.albumRepository = albumRepository;
+        this.grupoRepository = grupoRepository;
+    }
+
+    public ResponseEntity<Album> crearAlbum(Album album){
+        if(grupoRepository.findById(String.valueOf(Integer.valueOf(album.getGrupoId()))).isPresent()){
+            albumRepository.save(album);
+            return ResponseEntity.status(HttpStatus.CREATED).body(album);
+        }
+        return null;
+    }
 }
