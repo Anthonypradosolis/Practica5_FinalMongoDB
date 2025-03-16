f4d5573d7f846757b62be9822c7cf6b8bc6bd91f
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 11:39:16 +0100

    cambiamos una variable

--- src/main/java/com/example/demo/repository/GrupoRepository.java
+++ src/main/java/com/example/demo/repository/GrupoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GrupoRepository extends MongoRepository<Grupo,String> {
+public interface GrupoRepository extends MongoRepository<Grupo,Integer> {
 }
