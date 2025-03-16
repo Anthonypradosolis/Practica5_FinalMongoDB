385ea0cb1756e74ebc74f8381fe5a3026acfa703
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 11:20:55 +0100

    cambio un parametro a Integer

--- src/main/java/com/example/demo/repository/AlbumRepository.java
+++ src/main/java/com/example/demo/repository/AlbumRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlbumRepository extends MongoRepository<Album,String> {
+public interface AlbumRepository extends MongoRepository<Album,Integer> {
 }
