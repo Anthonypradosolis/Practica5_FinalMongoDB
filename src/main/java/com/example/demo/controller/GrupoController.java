17d4489a812084693b360dad5c15a0dcfbc7fa79
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 12:29:02 +0100

    creamos el metodo actualizarGrupo

--- src/main/java/com/example/demo/controller/GrupoController.java
+++ src/main/java/com/example/demo/controller/GrupoController.java
@@ -4,10 +4,7 @@ import com.example.demo.model.Grupo;
 import com.example.demo.service.GrupoServices;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/mongo/grupo")
@@ -23,5 +20,11 @@ public class GrupoController {
     @PostMapping("/crear")
     public ResponseEntity<Grupo> crearGrupo(@RequestBody Grupo grupo){
         return grupoServices.crearGrupo(grupo);
+    }
+
+    @Operation(summary = "Actualiza un grupo")
+    @PutMapping("/actualizar/{_id}")
+    public ResponseEntity<Grupo> actulizarGrupo(@PathVariable Integer _id,@RequestBody Grupo novoGrupo){
+        return grupoServices.actulizarGrupo(_id, novoGrupo);
     }
 }
