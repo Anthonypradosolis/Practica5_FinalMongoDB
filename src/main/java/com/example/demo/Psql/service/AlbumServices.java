a19ad31a95d19e42913de95c9b4aa6064f8f5aa9
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 13:49:35 +0100

    creamos las variables y un constructor

--- src/main/java/com/example/demo/Psql/service/AlbumServices.java
+++ src/main/java/com/example/demo/Psql/service/AlbumServices.java
@@ -1,4 +1,21 @@
 package com.example.demo.Psql.service;
 
+import com.example.demo.Psql.ComunicacionMongo.ComunicacionMongo;
+import com.example.demo.Psql.repository.AlbumRepository;
+import com.example.demo.Psql.repository.GrupoRepository;
+
 public class AlbumServices {
+
+    private final AlbumRepository albumRepository;
+    private final GrupoRepository grupoRepository;
+    private final ComunicacionMongo comunicacionMongo;
+
+    public AlbumServices(AlbumRepository albumRepository, GrupoRepository grupoRepository, ComunicacionMongo comunicacionMongo) {
+        this.albumRepository = albumRepository;
+        this.grupoRepository = grupoRepository;
+        this.comunicacionMongo = comunicacionMongo;
+    }
+
+
+
 }
