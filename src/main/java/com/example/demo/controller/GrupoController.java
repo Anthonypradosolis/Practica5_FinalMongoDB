9d8a1696420e28b3b9b229e99e8043cfd8417a61
Apradosolis <apradosolis@danielcastelao.org>
2025-03-16 12:23:58 +0100

    definimos una variable, el constructor y un metodo

--- src/main/java/com/example/demo/controller/GrupoController.java
+++ src/main/java/com/example/demo/controller/GrupoController.java
@@ -1,4 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Grupo;
+import com.example.demo.service.GrupoServices;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mongo/grupo")
 public class GrupoController {
+
+    private final GrupoServices grupoServices;
+
+    public GrupoController(GrupoServices grupoServices) {
+        this.grupoServices = grupoServices;
+    }
+
+    @Operation(summary = "Crea un grupo")
+    @PostMapping("/crear")
+    public ResponseEntity<Grupo> crearGrupo(@RequestBody Grupo grupo){
+        return grupoServices.crearGrupo(grupo);
+    }
 }
